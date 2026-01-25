4bbd297adc0790aec77474ed7408a73609978dd8
Alisson Aragão <alissondapsn@hotmail.com>
2026-01-25 15:48:25 -0300

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -7,7 +7,9 @@ const Home: React.FC = () => {
     <div className="flex flex-col items-center animate-fade-in">
       <header className="w-full text-center flex flex-col items-center mb-8">
         <div className="mb-4 text-primary animate-bounce">
-          <span className="material-symbols-outlined text-5xl">favorite_sharp</span>
+
+          <img src="https://i.ibb.co/LzGdb9Hp/b9067b33984ae88ed69cdb4b04bbb614.jpg" alt="b9067b33984ae88ed69cdb4b04bbb614" border="0">
+          {/* <span className="material-symbols-outlined text-5xl">favorite_sharp</span> */}
         </div>
         <h1 className="text-[#2c1810] font-serif text-[40px] md:text-[48px] font-bold leading-[1.1] mb-2 tracking-tight">
           Aniversário e 
